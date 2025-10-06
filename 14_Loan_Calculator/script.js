a2c688315edddf979ab39cf1b88e3e5b7c0f0c77
Kranthi Chintu <kranthichintu6600@gmail.com>
2025-10-06 12:40:19 +0530

    updated

---
+++ 14_Loan_Calculator/script.js
@@ -0,0 +1,13 @@
+function calculateLoan(){
+    loanAmountValue = document.getElementById('loan-amount').value
+
+    interestRateValue = document.getElementById('interest-rate').value
+    
+    monthsToPayValue = document.getElementById('months-to-pay').value
+
+    interest = (loanAmountValue * (interestRateValue * 0.01)) / monthsToPayValue
+
+    monthlyPayment = (loanAmountValue / monthsToPayValue + interest);
+
+    document.getElementById('payment').innerHTML = `Monhtly Payment: ${monthlyPayment}`;
+}
